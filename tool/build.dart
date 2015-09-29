096de46853c8936902caafdd556189604d461565
Kenneth Endfinger <kaendfinger@gmail.com>
2015-09-29 18:20:25 -0400

    Add support for configuration variables.

--- tool/build.dart
+++ tool/build.dart
@@ -3,10 +3,15 @@ import "util.dart";
 main(List<String> argv) async {
   var links = await loadJsonDirectoryList("data/links");
   var typeRules = await loadJsonDirectoryMap("data/types");
+  var mixins = await loadJsonDirectoryMap("data/mixins");
+  var config = await readJsonFile("data/config.json");
 
   for (Map l in links) {
     mergeConfigurationTypes(typeRules, "type", l);
+    mergeConfigurationTypes(mixins, "mixins", mixins);
   }
+
+  crawlDataAndSubstituteVariables(links, config);
 
   var revs = await readJsonFile("data/revs.json");
 
--- tool/util.dart
+++ tool/util.dart
@@ -193,6 +193,11 @@ Future makeZipFile(String target) async {
 
 Future<List<dynamic>> loadJsonDirectoryList(String path, {bool strict: false}) async {
   var dir = new Directory(path);
+
+  if (!(await dir.exists())) {
+    return [];
+  }
+
   var out = [];
   await for (FileSystemEntity entity in dir.list()) {
     if (entity is! File) continue;
@@ -216,6 +221,11 @@ Future<List<dynamic>> loadJsonDirectoryList(String path, {bool strict: false}) a
 
 Future<Map<String, dynamic>> loadJsonDirectoryMap(String path, {bool strict: false}) async {
   var dir = new Directory(path);
+
+  if (!(await dir.exists())) {
+    return {};
+  }
+
   var out = {};
   await for (FileSystemEntity entity in dir.list()) {
     if (entity is! File) continue;
@@ -241,11 +251,52 @@ Future<Map<String, dynamic>> loadJsonDirectoryMap(String path, {bool strict: fal
 
 void mergeConfigurationTypes(Map<String, Map<String, dynamic>> types, String field, Map input) {
   if (input == null) return;
-  var type = input[field];
-  if (type is String && types.containsKey(type)) {
-    var original = new Map.from(input);
-    input.clear();
-    input.addAll(merge(original, types[type]));
+  var typeList = input[field];
+  if (typeList is String) {
+    typeList = [typeList];
+  }
+
+  if (typeList is! List) {
+    return;
+  }
+
+  for (String type in typeList) {
+    if (type is String && types.containsKey(type)) {
+      var original = new Map.from(input);
+      input.clear();
+      input.addAll(merge(original, types[type]));
+    }
+  }
+}
+
+void crawlDataAndSubstituteVariables(input, Map<String, dynamic> variables) {
+  String handleString(String input) {
+    for (var key in variables.keys) {
+      var value = variables[key];
+      input = input.replaceAll(r"${" + key + "}", value);
+    }
+    return input;
+  }
+
+  if (input is List) {
+    for (var i = 0; i < input.length; i++) {
+      var e = input[i];
+      if (e is! String) {
+        crawlDataAndSubstituteVariables(e, variables);
+      } else {
+        input[i] = handleString(e);
+      }
+    }
+  } else if (input is Map) {
+    for (var key in input.keys.toList()) {
+      var value = input[key];
+
+      if (value is! String) {
+        crawlDataAndSubstituteVariables(value, variables);
+      } else {
+        input[key] = handleString(value);
+      }
+    }
   }
 }
 
